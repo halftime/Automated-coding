ee78b69bca9fe4155b955562836aa13389f70b9a
halftime <60786122+halftime@users.noreply.github.com>
2023-09-10 19:44:04 +0200

    Update 1-drop-SQL-db.bat
    taskkill sqlservr.exe to kill open connection before query

--- .NET EntityFrameworkCore/1-drop-SQL-db.bat
+++ .NET EntityFrameworkCore/1-drop-SQL-db.bat
@@ -1,6 +1,9 @@
 rem Set MSSQLLocalDB name to drop
 set sqlDBName=SQLDatbaseNameHere
 
+rem taskkill sqlservr.exe to close possible connection in SQL Server GUI
+taskkill /IM "sqlservr.exe" /F
+
 rem CD to SSMS folder
 cd "%ProgramFiles(x86)%\Microsoft SQL Server Management Studio 19\Common7\IDE"
 
